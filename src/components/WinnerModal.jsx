42006027f9458d930d9154538c6a088536979d4d
Nicolas sarchi <nicolassarchipico@gmail.com>
2023-11-01 11:24:12 -0500

    feat: :sparkles:

--- src/components/WinnerModal.jsx
+++ src/components/WinnerModal.jsx
@@ -1,29 +1,23 @@
-import { Square } from "./Square"
+import { Square } from "./Square";
 // eslint-disable-next-line react/prop-types
-export function WinnerModal ({winner, resetGame})
-{
-    if (!winner) {
-        return null
-    }
+export function WinnerModal({ winner, resetGame }) {
+  if (winner === null) {
+    return null;
+  }
 
-    const winnerText =  winner === false ? "Empate": 'Ganó:'
-    return (    
-            <section className="winner">
-              <div className="text">
-                <h2>
-                  {
-                   {winnerText}
-                  }
-                </h2>
-                <header className="win">
-                  {
-                    winner && <Square>{winner}</Square>
-                  }
-                </header>
-                <footer>
-                  <button onClick={resetGame}>Empezar de nuevo</button>
-                </footer>
-              </div>
-            </section>
-    )
+  const winnerText = winner === false ? "Empate" : "Ganó:";
+  return (
+    <section className="winner">
+      <div className="text">
+        <h2>{winnerText}</h2>
+        <header className="win">
+          {winner  && <Square>{winner}</Square>}
+        </header>
+
+        <footer>
+          <button onClick={resetGame}>Empezar de nuevo</button>
+        </footer>
+      </div>
+    </section>
+  );
 }
